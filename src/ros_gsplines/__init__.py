58895129cf0759029b419e255ac2b9156d35b4e4
Rafael Rojas <rafael.rojas@fraunhofer.it>
2021-02-14 17:52:07 +0100

    [ros_gsplines] map gspline to gspline message

--- src/ros_gsplines/__init__.py
+++ src/ros_gsplines/__init__.py
@@ -4,3 +4,4 @@ This file makes available the submodules
 from .trajectory_message import gspline_to_joint_trajectory_message
 from .trajectory_message import gspline_to_follow_joint_trajectory_goal
 from .gspline_message import gspline_to_joint_gspline_trajectory
+from .gspline_message import joint_gspline_trajectory_to_gspline
--- src/ros_gsplines/gspline_message.py
+++ src/ros_gsplines/gspline_message.py
@@ -1,6 +1,7 @@
 
 import std_msgs.msg
 from gsplines.services import piecewise2json
+from gsplines.services import json2piecewise
 from .msg import JointGSplineTrajectory
 import rospy
 
@@ -15,4 +16,9 @@ def gspline_to_joint_gspline_trajectory(_trj, _joint_names, _rate):
     result.joint_names = _joint_names
     result.json_representation = piecewise2json(_trj)
     result.rate = _rate
+    return result
+
+
+def joint_gspline_trajectory_to_gspline(_jgt):
+    result = json2piecewise(_jgt.json_representation)
     return result
